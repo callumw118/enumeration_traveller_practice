62cfe5c8169993db098a8f47a9ab296d7f4e4f7e
Callum Wolfe <wolfe.callum@gmail.com>
2020-08-19 15:53:03 +0100

    Adds code for getting the start and end journeys

---
+++ models/traveller.js
@@ -0,0 +1,36 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  const startLocations = this.journeys.map((journey) => {
+    return journey.startLocation;
+  });
+  return startLocations;
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  const endLocations = this.journey.map((journey) => {
+    return journey.endLocation;
+  });
+  return endLocations;
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+
+};
+
+
+module.exports = Traveller;
